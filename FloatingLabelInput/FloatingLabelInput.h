01bff9aebc5b13d9cf96aba446e6d92661a305a4
Mark Boleigha <donhoenix@gmail.com>
2019-11-05 10:36:49 +0100

    added source files

---
+++ FloatingLabelInput/FloatingLabelInput.h
@@ -0,0 +1,19 @@
+//
+//  FloatingLabelInput.h
+//  FloatingLabelInput
+//
+//  Created by Sprinthub on 05/11/2019.
+//  Copyright © 2019 Sprinthub Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FloatingLabelInput.
+FOUNDATION_EXPORT double FloatingLabelInputVersionNumber;
+
+//! Project version string for FloatingLabelInput.
+FOUNDATION_EXPORT const unsigned char FloatingLabelInputVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloatingLabelInput/PublicHeader.h>
+
+
